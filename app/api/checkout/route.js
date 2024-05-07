38a17289dfc79ee2054dd5f2f8f3005788de599b
Cintia Ruiz <cintiaruizv@gmail.com>
2024-05-07 12:35:57 -0700

    adding application_fee to stripe session

--- app/api/checkout/route.js
+++ app/api/checkout/route.js
@@ -1,34 +1,67 @@
-/* import { NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 const STRIPE_SECRET_KEY = process.env.STRIPE_SECRET_KEY
 import Stripe from 'stripe'
 
 const stripe = new Stripe(STRIPE_SECRET_KEY)
 
-export async function POST(request){
+export async function POST(request) {
 
-    const body = await request.json()
-    console.log("🚀 ~ POST ~ body:", body)
+  const body = await request.json()
+  console.log("🚀 ~ POST ~ body:", body)
 
-    const session = await stripe.checkout.sessions.create({
-        success_url:"http://localhost:3000/success",
-        line_items:[ //con todos los productos que queremos cobrar
-            {
-                price_data: {
-                    currency:"usd",
-                    product_data:{
-                        name:body.name,
-                        images: [body.image]
-                    },
-                    unit_amount: body.price
-                },
-                quantity:1
-            }
-        ],
-        metadata:{
-            productId: body.id
+  const session = await stripe.checkout.sessions.create({
+    success_url: "http://localhost:3000/success",
+    line_items: [ //con todos los productos que queremos cobrar
+      {
+        price_data: {
+          currency: "usd",
+          product_data: {
+            name: body.name,
+            images: [body.image]
+          },
+          unit_amount: body.price
         },
-        mode:"payment"
-    })
-    await createPaymentOnDB()
-    return NextResponse.json(session)
-} */
+        quantity: 1
+      }
+    ],
+    payment_intent_data: {
+      application_fee_amount: 123,
+    },
+    mode: "payment"
+  },
+    {
+      stripeAccount: 'acct_1PDFX8BCFLfeZNkg',
+    }
+  )
+  //await createPaymentOnDB()
+  return NextResponse.json(session)
+}
+
+
+// Set your secret key. Remember to switch to your live secret key in production.
+// See your keys here: https://dashboard.stripe.com/apikeys
+/* 
+const session = await stripe.checkout.sessions.create(
+  {
+    line_items: [
+      {
+        price_data: {
+          currency: 'usd',
+          product_data: {
+            name: 'T-shirt',
+          },
+          unit_amount: 1000,
+        },
+        quantity: 1,
+      },
+    ],
+    payment_intent_data: {
+      application_fee_amount: 123,
+    },
+    mode: 'payment',
+    success_url: 'https://example.com/success?session_id={CHECKOUT_SESSION_ID}',
+  },
+  {
+    stripeAccount: '{{CONNECTED_ACCOUNT_ID}}',
+  }
+); */
--- app/page.js
+++ app/page.js
@@ -21,6 +21,9 @@ export default function Home() {
       <button className="bg-pink-100 rounded-md w-44 p-5">
         <Link href="/reserves">Go to you reserve detail</Link>
       </button>
+      <button className="bg-pink-100 rounded-md w-44 p-5">
+        <Link href="/checkout">CHECKOUT</Link>
+      </button>
     </div>
   );
 }
